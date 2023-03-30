711311fe81d52828ab2eb8f4ccbe95000b981c11
Ramanujam Gond <ramanujamgond@gmail.com>
2023-03-30 20:45:26 +0530

    feat: initial project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const title = 'Blog Post';
+    const body = 'This is my blog post';
+
+    const comments = [
+        { id: 1, text: 'Comment one' },
+        { id: 2, text: 'Comment two' },
+        { id: 3, text: 'Comment three' },
+    ]
+    return (
+        <div className="container">
+            <h1>{title.toUpperCase()}</h1>
+            <p>{body}</p>
+            {Math.random() * (5 + 5)}
+            {/* {comments?.map((commentsValue) => {
+                return (
+                    <div key={commentsValue?.id}>
+                        {commentsValue?.text}
+                    </div>
+                )
+            })} */}
+
+            <div className='comments'>
+                <h3>Comments ({comments.length})</h3>
+                <ul>
+                    {comments?.map((comment) => (
+                        <li key={comment?.id}>{comment?.text}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 export default App;
